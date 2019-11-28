0f5b95580a1954882c83d4bf602b67cde5ea91a1
avnermax <avnerpaulo.mg@gmail.com>
2019-11-28 21:29:46 -0200

    update força bruta

--- Software/funcoes.c
+++ Software/funcoes.c
@@ -33,10 +33,14 @@ Info * interpretaEntrada(int *a, char *arq){
 				fscanf(data, "%d %d\n", &b, &c);
 				info[x].quantPlanetas = b; // Guarda a quantidade de planetas.
 				info[x].quantSaltos = c; // Guarda a quantidade de saltos possíveis.
-				info[x].distancias = (int*) malloc((b + 1) * sizeof(int)); // Aloca vetor para guardar os valores de distâncias.
-				for(y = 0; y < b + 1; y++){
+
+				// Aloca matriz para guardar os valores de distâncias.
+				info[x].matDist = (int**) malloc((b + 2) * sizeof(int*));
+				for(y = 0; y < b + 2; y++) info[x].matDist[y] = (int*) calloc((b + 2), sizeof(int));
+
+				for(y = 1; y < b + 2; y++){
 					fscanf(data, "%d\n", &d);
-					info[x].distancias[y] = d; // Guarda valores de cada distancia disponível, para uma configuração.
+					info[x].matDist[y - 1][y] = d; // Guarda valores de cada distancia disponível, para uma configuração.
 				}
 			}
 		}else if(cLinha == 0){
@@ -55,67 +59,22 @@ int fat(int n){
 	return (n == 1 || n == 0) ? 1: n * fat(n - 1);
 }
 
-void combinacao(int **mat, int *k, int *vetor, int indice, int *data, int inicio, int fim, int r){
-	int i, j;
+void combinacao(int **mat, int *somaDist, int indice, int inicio, int fim, int r){
+	int i, soma = 0;
 
 	// Caso base da recursão.
 	if (indice == r){
-		for (j = 0; j < r; j++){
-			mat[*k][j] = data[j];
-			printf("%d ", mat[*k][j]);
-		}
-		printf("\n");
+		if(soma >= *somaDist) *somaDist = soma;
 
-		*k += 1;
 		return;
 	}
 
 	// Controla os passos da permutação.
-	for(i = inicio; i <= fim && fim-i+1 >= r-indice; i++){
-		data[indice] = vetor[i];
-		combinacao(mat, k, vetor, indice + 1, data, i + 1, fim, r);
+	for(i = inicio; i < fim && fim-i+1 >= r-indice; i++){
+		soma = mat[i-1][i] + mat[i][i+1];
+		printf("soma:%d\n", soma);
+		combinacao(mat, somaDist, indice + 1, i + 1, fim, r);
 	}
-}
-
-void buscaCombinacoes(int **mat, int *k, int *vetor, int n, int r, int c){
-	int *data;
-
-	// Vetor que contém os itens a serem guardados na matriz.
-	data = (int*) malloc(r * sizeof(int));
-
-	// Inicia a recursão que busca as combinações.
-	combinacao(mat, k, vetor, 0, data, 0, n - 1, r);
-}
-
-void obtemDados(int x, Info *info){
-	int y, q, num;
-	float numComb;
-
-	num = 0;
-	q = (info[x].quantPlanetas + 1) - info[x].quantSaltos;
-
-	// Determina o número máximo de combinações para uma configuração a ser testada.
-	int fatA = fat(info[x].quantPlanetas + 1);
-	int fatB = fat(info[x].quantSaltos);
-	int fatC = fat(q);
-	numComb = fatA / (fatB * fatC);
-
-	// Aloca matriz que armazena todas as combinações de uma configurações.
-	info[x].matriz = (int**) malloc((int)numComb * sizeof(int*));
-	for(y = 0; y < (int)numComb; y++) info[x].matriz[y] = calloc(q, sizeof(int));
-
-	buscaCombinacoes(info[x].matriz, &num, info[x].distancias, info[x].quantPlanetas + 1, q, (int)numComb);
-}
-
-int encontraMaior(int menor, Info *info){
-	int x, maior;
-
-	maior = menor;
-	for(x = 0; x < info[x].quantPlanetas+1; x++){
-		if(menor >= maior) maior = menor;
-	}
-
-	return maior;
 }
 
 /*----------------------------------------*/
@@ -123,74 +82,72 @@ int encontraMaior(int menor, Info *info){
 /*-------------- ALGORITMOS --------------*/
 
 /* FORÇA BRUTA */
-int aplicaFB(int x, Info *info){
-	int menor;
-
-	//
-
-	return menor = 0; // valor 0 usado para evitar bug
-}
-
 void executaForcaBruta(int n, Info *info){
-	int x, menor;
+	int x;
 
 	// Faz todos os testes em força bruta.
 	for(x = 0; x < n; x++){
-		obtemDados(x, info);
+		int q;
+		float numComb;
+
+		q = info[x].quantPlanetas - info[x].quantSaltos;
+
+		// Determina o número máximo de combinações para uma configuração a ser testada.
+		int fatA = fat(info[x].quantPlanetas);
+		int fatB = fat(info[x].quantSaltos);
+		int fatC = fat(q);
+		numComb = fatA / (fatB * fatC);
+
+		// Inicia a recursão que busca as combinações.
+		combinacao(info[x].matDist, &info[x].resultado, 0, 1, info[x].quantPlanetas+1, (int)numComb);
+
+		for(int i=0; i<info[x].quantPlanetas+2; i++){
+			for(int j=0; j<info[x].quantPlanetas+2; j++){
+				if(info[x].matDist[i][j] != 0){
+					printf("\033[1;31m%d ", info[x].matDist[i][j]);
+				}else{
+					printf("\033[0m%d ", info[x].matDist[i][j]);
+				}
+			}
+			printf("\n");
+		}
+		printf("\n");
 
 		// A partir das combinações obtidas, precisamos selecionar a que a soma dê a menor distância correta, usando FB.
-		menor = aplicaFB(x, info);
+		// menor = aplicaFB(x, info);
 
 		// Após encontrar salto de menor valor, busca agora o maior salto entre as distancias originais.
-		info[x].resultado = encontraMaior(menor, info); // Grava o resultado na variável de saída.
+		// info[x].resultado = encontraMaior(menor, info); // Grava o resultado na variável de saída.
 	}
 }
 
 /* SOLUÇÃO GULOSA */
-int aplicaAG(int x, Info *info){
-	int menor;
-
-	//
-
-	return menor = 0; // valor 0 usado para evitar bug
-}
-
 void executaAlgGuloso(int n, Info *info){
-	int x, menor;
+	int x;
 
 	// Faz todos os testes na solução gulosa.
 	for(x = 0; x < n; x++){
-		obtemDados(x, info);
 
 		// A partir das combinações obtidas, precisamos selecionar a que a soma dê a menor distância correta, usando AG.
-		menor = aplicaAG(x, info);
+		// menor = aplicaAG(x, info);
 
 		// Após encontrar salto de menor valor, busca agora o maior salto entre as distancias originais.
-		info[x].resultado = encontraMaior(menor, info); // Grava o resultado na variável de saída.
+		// info[x].resultado = encontraMaior(menor, info); // Grava o resultado na variável de saída.
 	}
 }
 
 /* PROGRAMAÇÃO DINÂMICA */
-int aplicaPD(int x, Info *info){
-	int menor;
-
-	//
-
-	return menor = 0; // valor 0 usado para evitar bug
-}
-
 void executaProgDinamica(int n, Info *info){
-	int x, menor;
+	int x;
 
 	// Faz todos os testes em programação dinâmica.
 	for(x = 0; x < n; x++){
-		obtemDados(x, info);
 
 		// A partir das combinações obtidas, precisamos selecionar a que a soma dê a menor distância correta, usando PD.
-		menor = aplicaPD(x, info);
+		// menor = aplicaPD(x, info);
 
 		// Após encontrar salto de menor valor, busca agora o maior salto entre as distancias originais.
-		info[x].resultado = encontraMaior(menor, info); // Grava o resultado na variável de saída.
+		// info[x].resultado = encontraMaior(menor, info); // Grava o resultado na variável de saída.
 	}
 }
 
--- Software/funcoes.h
+++ Software/funcoes.h
@@ -4,26 +4,18 @@
 #include <unistd.h>
 
 typedef struct T_Info{
-	int *distancias, quantPlanetas, quantSaltos;
-	int **matriz, resultado;
+	int **matDist, quantPlanetas, quantSaltos;
+	int resultado;
 }Info;
 
 /* funcoes.c */
 void imprimeResultado(int n, Info *info);
 Info * interpretaEntrada(int *n, char *entrada);
 int fat(int n);
-void combinacao(int **mat, int *k, int *vetor, int indice, int *data, int inicio, int fim, int r);
-void buscaCombinacoes(int **mat, int *k, int *vetor, int n, int r, int c);
-void obtemDados(int x, Info *info);
-int encontraMaior(int menor, Info *info);
+void combinacao(int **mat, int *somaDist, int indice, int inicio, int fim, int r);
 
-int aplicaFB(int x, Info *info);
 void executaForcaBruta(int n, Info *info);
-
-int aplicaAG(int x, Info *info);
 void executaAlgGuloso(int n, Info *info);
-
-int aplicaPD(int x, Info *info);
 void executaProgDinamica(int n, Info *info);
 
 /* arg.c */
